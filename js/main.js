ac7e0e4c80401e5c70dbed00ca850de3ad255300
dwf <me@joer.eu>
2013-11-25 15:51:23 +0100

    phonegap

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ var app = {
     
     renderHomeView: function() {
     var html =
-            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='header'><h1>Accueil Ambroisie</h1></div>" +
             "<div class='search-view'>" +
             "<input class='search-key'/>" +
             "<ul class='employee-list'></ul>" +
